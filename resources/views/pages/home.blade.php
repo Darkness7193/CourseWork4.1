c651c8f37cde7a28b26b852433af9a08618af864
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-03 01:35:21 +0300

    0.229 home now have good look. Again

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,26 +3,35 @@
 
 <!-- imports: -->
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+    @vite(['resources/css/breeze-base.css'])
 
 
 
 <body>
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <button onclick="window.location='{{ route('product_moves.purchases_crud') }}'"> Покупки </button>
-                    <button onclick="window.location='{{ route('product_moves.sales_crud') }}'"> Продажи </button>
-                    <button onclick="window.location='{{ route('product_moves.inner_moves_crud') }}'"> Внутренние движения </button>
-
-                    <button onclick="window.location='{{ route('product_moves.totals_by_move_type') }}'"> Отчет по типам движений </button>
-                    <button onclick="window.location='{{ route('product_moves.totals_by_month') }}'"> Отчет по месяцам </button>
-
-                    <button onclick="window.location='{{ route('products.crud') }}'"> Товары </button>
-                    <button onclick="window.location='{{ route('storages.crud') }}'"> Склады </button>
+    <div class="py-12 vertical-arrange">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <x-card>
+                <div class="vertical-arrange">
+                    <x-responsive-nav-link onclick="window.location='{{ route('product_moves.purchases_crud') }}'">{{ __('Покупки') }}</x-responsive-nav-link>
+                    <x-responsive-nav-link onclick="window.location='{{ route('product_moves.sales_crud') }}'">{{ __('Продажи') }}</x-responsive-nav-link>
+                    <x-responsive-nav-link onclick="window.location='{{ route('product_moves.inner_moves_crud') }}'">{{ __('Внутренние движения') }}</x-responsive-nav-link>
                 </div>
-            </div>
+            </x-card>
+
+            <x-card>
+                <div class="vertical-arrange">
+                    <x-responsive-nav-link onclick="window.location='{{ route('product_moves.totals_by_move_type') }}'">{{ __('Отчет по типам движений') }}</x-responsive-nav-link>
+                    <x-responsive-nav-link onclick="window.location='{{ route('product_moves.totals_by_month') }}'">{{ __('Отчет по месяцам') }}</x-responsive-nav-link>
+                </div>
+            </x-card>
+
+            <x-card>
+                <div class="vertical-arrange">
+                    <x-responsive-nav-link onclick="window.location='{{ route('products.crud') }}'">{{ __('Товары') }}</x-responsive-nav-link>
+                    <x-responsive-nav-link onclick="window.location='{{ route('storages.crud') }}'">{{ __('Склады') }}</x-responsive-nav-link>
+                </div>
+            </x-card>
         </div>
     </div>
 </x-app-layout>
