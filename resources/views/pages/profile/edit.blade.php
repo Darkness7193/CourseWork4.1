344810bdc15ffb7cbdb726abe2cb4eca901f54c0
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-03 02:13:15 +0300

    0.229 :4

--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -1,5 +1,7 @@
+@vite(['resources/css/breeze-base.css'])
+
+
 <x-app-layout>
-    @vite(['resources/css/breeze-base.css'])
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
