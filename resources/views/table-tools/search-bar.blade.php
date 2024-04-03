d5019f943b764a0e99ec51c925470032de301115
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-04 00:36:04 +0300

    0.246 tablewise and fieldwise search now again remember old values

--- resources/views/table-tools/search-bar.blade.php
+++ resources/views/table-tools/search-bar.blade.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="{{ asset('css/table-tools/search-bar.css') }}">
 
 
-<!-- f($CrudModel, $search_target, $view_fields, $headers): -->
+<!-- f($CrudModel, $search_targets, $view_fields, $headers): -->
 <form class="search-bar vertical-center"
     method="post"
     action="{{ route('set_filter', ['previous_route' => Route::current()->getName()]) }}"
@@ -10,15 +10,15 @@
 
     <div class="default-input-wrapper"><button name="action" value="search"></button></div>
 
-    @include('table-tools.fieldwise-search-btn', compact('view_fields', 'headers'))
+    @include('table-tools.fieldwise-search-btn', compact('view_fields', 'headers', 'search_targets'))
     <button class="icon anti-search-btn" type="submit" name="action" value="un_search"></button>
     <button class="icon search-btn" type="submit" name="action" value="search"></button>
 
     <input class="search-input"
         name="tablewise_search_target"
+        value="{{ $search_targets['tablewise'] ?? '' }}"
         type="text"
         placeholder="Фильтр"
         onfocus="this.select();"
-        autofocus
     >
 </form>
