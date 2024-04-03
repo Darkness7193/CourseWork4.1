fcb809e7a7b47bd49675e1108476aa3f2f16a740
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-03 23:02:52 +0300

    0.244 :3

--- public/js/of_crud-table/auto_table_input_refocus.js
+++ public/js/of_crud-table/auto_table_input_refocus.js
@@ -8,7 +8,7 @@ function auto_table_input_refocus(table) {
 
         if (document.activeElement.tagName === 'INPUT') {
             let [row_index, cell_index] = row_cell_indexes(document.activeElement)
-            let has_next_row = ()=>{ return row_index+1 <= table.rows.length-1 }
+            let has_next_row = ()=>{ return row_index+1 <= table.rows.length-2 }
             let has_next_col = ()=>{ return cell_index+1 <= table.dataset.viewFields.split(',').length-1 }
 
             if (has_next_row()) {
