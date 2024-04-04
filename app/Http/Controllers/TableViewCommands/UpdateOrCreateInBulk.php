efa7f17041696deeeba85d042391b76f45a01bbe
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-04 16:56:45 +0300

    0.247.1 update_or_create_in_bulk.php encapsulation. Preparations to give to all cruds their own UpdateOrCreateInBulk methods

--- app/Http/Controllers/TableViewCommands/UpdateOrCreateInBulk.php
+++ app/Http/Controllers/TableViewCommands/UpdateOrCreateInBulk.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\TableViewCommands;
 
+include_once(app_path().'/sql/helpers/update_or_create_in_bulk.php');
+
 use App\Models\ProductMove;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -11,18 +13,6 @@ class UpdateOrCreateInBulk extends Controller
 {
     public function __invoke(Request $request): void
     {
-        $fillable_count = count((new $request->CrudModel)->getFillable());
-        foreach ($request->updated_rows as $row_id => $updated_cells)
-        {
-            $exist_purchase = $request->CrudModel::find($row_id);
-            if ($exist_purchase) {
-                $exist_purchase->update($updated_cells);
-            } else {
-                $new_purchase = array_merge($request->no_view_fields, $updated_cells);
-                if (count($new_purchase) === $fillable_count) {
-                    $request->CrudModel::create($new_purchase);
-                }
-            }
-        }
+        update_or_create_in_bulk($request->CrudModel, $request->updated_rows, $request->no_view_fields);
     }
 }
