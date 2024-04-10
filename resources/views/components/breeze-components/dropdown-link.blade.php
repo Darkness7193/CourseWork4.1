3c235b3b5b1829bc43e4b5956969c96be22bdbca
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-10 03:45:57 +0300

    0.249 :5

--- resources/views/components/breeze-components/dropdown-link.blade.php
+++ resources/views/components/breeze-components/dropdown-link.blade.php
@@ -5,4 +5,4 @@
 </style>
 
 
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out prevent-expand-from-parent']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out prevent-expand-from-parent']) }} style="width: inherit">{{ $slot }}</a>
