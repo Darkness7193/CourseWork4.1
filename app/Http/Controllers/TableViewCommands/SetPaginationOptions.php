1febdda054d7b286dbdf9f1bb951e0b2db0138ab
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-10 10:27:49 +0300

    0.252 pagination current page now are adjust with change of per_page. It try to stay near first item of previous current page

--- app/Http/Controllers/TableViewCommands/SetPaginationOptions.php
+++ app/Http/Controllers/TableViewCommands/SetPaginationOptions.php
@@ -10,12 +10,23 @@ use Illuminate\Routing\Controller;
 
 
 
+function adjust_current_page($current_page, $per_page) {
+    if ($current_page !== session('current_page') || !(session('current_page') && session('per_page'))) {
+        return $current_page;
+    }
+
+    $first_item_of_page = ((int)session('current_page')-1)*(int)session('per_page') + 1;
+    return intdiv($first_item_of_page, $per_page) + 1;
+}
+
+
 class SetPaginationOptions extends Controller
 {
     public function __invoke(Request $request) {
         session_setif([
+            'pagination_total' => $request->total,
             'per_page' => $request->per_page,
-            'current_page' => $request->current_page
+            'current_page' => adjust_current_page($request->current_page, $request->per_page)
         ]);
 
         return to_route($request->previous_route);
--- app/sql/helpers/paginate.php
+++ app/sql/helpers/paginate.php
@@ -6,6 +6,19 @@ use Illuminate\Support\Collection;
 
 
 
+function adjust_current_page($current_page, $per_page) {
+    if (!(session('current_page') && session('per_page'))) {
+        return $current_page;
+    }
+
+    $first_item_of_page = ((int)session('current_page')-1)*(int)session('per_page') + 1;
+    dump($first_item_of_page);
+    dump($per_page);
+    dump(intdiv($first_item_of_page, $per_page) + 1);
+    return intdiv($first_item_of_page, $per_page) + 1;
+}
+
+
 function paginate(&$query, $per_page=null, $current_page=1, $columns=['*'], $page_name='page')
 {
     if ($query === null) { $arr=[]; return paginate_array($arr, 1); }
