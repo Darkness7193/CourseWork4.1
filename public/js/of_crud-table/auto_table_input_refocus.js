3e119cb478edba4f8008eb4772a8bc8871c211b0
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-10 11:03:24 +0300

    0.252 :4 auto_table_input_refocus.js how have row_refocus() by shift hold

--- public/js/of_crud-table/auto_table_input_refocus.js
+++ public/js/of_crud-table/auto_table_input_refocus.js
@@ -1,37 +1,76 @@
 import { row_cell_indexes } from "../helpers.js"
 
 
+function row_refocus(table) {
+    if (document.activeElement.tagName === 'INPUT') {
+        let [row_index, cell_index] = row_cell_indexes(document.activeElement)
+        let has_next_row = ()=>{ return row_index+1 <= table.rows.length-2 }
+        let has_next_column = ()=>{ return cell_index+1 <= table.dataset.viewFields.split(',').length-1 }
+
+        if (has_next_row()) {
+            row_index += 1
+        } else if (has_next_column()) {
+            cell_index += 1
+            row_index = 1
+        } else {
+            return
+        }
+
+        while (true) {
+            let lower_input = table.rows[row_index].cells[cell_index].children[0]
+
+            if (lower_input.tagName === 'INPUT') {
+                lower_input.select()
+                return
+            } else if (has_next_column()) {
+                cell_index += 1
+            } else {
+                return
+            }
+        }
+    }
+}
+
+
+function column_refocus(table) {
+    if (document.activeElement.tagName === 'INPUT') {
+        let [row_index, cell_index] = row_cell_indexes(document.activeElement)
+        let has_next_row = ()=>{ return row_index+1 <= table.rows.length-2 }
+        let has_next_column = ()=>{ return cell_index+1 <= table.dataset.viewFields.split(',').length-1 }
+
+        if (has_next_column()) {
+            cell_index += 1
+        } else if (has_next_row()) {
+            row_index += 1
+            cell_index = 0
+        } else {
+            return
+        }
+
+        while (true) {
+            let lower_input = table.rows[row_index].cells[cell_index].children[0]
+
+            if (lower_input.tagName === 'INPUT') {
+                lower_input.select()
+                return
+            } else if (has_next_column()) {
+                cell_index += 1
+            } else {
+                return
+            }
+        }
+    }
+}
+
+
 function auto_table_input_refocus(table) {
     let enter_key_code = 13
 
     table.addEventListener('keydown', (event)=>{ if (event.which === enter_key_code) {
-
-        if (document.activeElement.tagName === 'INPUT') {
-            let [row_index, cell_index] = row_cell_indexes(document.activeElement)
-            let has_next_row = ()=>{ return row_index+1 <= table.rows.length-2 }
-            let has_next_column = ()=>{ return cell_index+1 <= table.dataset.viewFields.split(',').length-1 }
-
-            if (has_next_row()) {
-                row_index += 1
-            } else if (has_next_column()) {
-                cell_index +=1
-                row_index = 1
-            } else {
-                return
-            }
-
-            while (true) {
-                let lower_input = table.rows[row_index].cells[cell_index].children[0]
-
-                if (lower_input.tagName === 'INPUT') {
-                    lower_input.select()
-                    return
-                } else if (has_next_column()) {
-                    cell_index +=1
-                } else {
-                    return
-                }
-            }
+        if (event.shiftKey) {
+            column_refocus(table)
+        } else {
+            row_refocus(table)
         }
     } })
 }
---
+++ public/js/of_crud-table/copy_input_value_by_hold.js
@@ -0,0 +1,3 @@
+function copy_input_value_by_hold() {
+
+}
