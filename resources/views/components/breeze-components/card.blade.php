d8eb638152219f499ea8d51ae8185b47e3044fa9
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-15 17:47:14 +0300

    0.264 almost pixel-perfect order and filter drop-downs. But dirty hack that I use is't good enough and have 1px error

--- resources/views/components/breeze-components/card.blade.php
+++ resources/views/components/breeze-components/card.blade.php
@@ -4,7 +4,7 @@
 
 @props(['style' => '', 'class' => ''])
 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 {{ $class }}" style="{{ $style }}">
-    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
             {{ $slot }}
         </div>
