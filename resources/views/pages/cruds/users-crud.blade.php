4abb8d580a6b2fd9f82634f64ee1cc48d973dcb8
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-04-17 13:43:43 +0300

    0.268 :1

--- resources/views/pages/cruds/users-crud.blade.php
+++ resources/views/pages/cruds/users-crud.blade.php
@@ -11,7 +11,6 @@
 
             <th><x-crud-components.activate-delete-btns-btn/></th>
         </tr>
-        @dump($paginator->items())
         @foreach (array_merge($paginator->items(), $filler_rows) as $user)
 
             <tr data-row-id="{{ $user->id }}">
