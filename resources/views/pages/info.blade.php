27d1889fab2875cc32676ce5903c79cdbfb8ec6e
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-05-31 17:32:21 +0300

    0.282.1

--- resources/views/pages/info.blade.php
+++ resources/views/pages/info.blade.php
@@ -11,9 +11,7 @@
 <x-app-layout>
     <x-card-list>
         <div>
-            <h2 class="card-header text-lg font-medium text-gray-900 dark:text-gray-100">
-                Справка
-            </h2>
+            <x-card-header text="Справка"/>
             <x-card>
                 фывафывафыв
             </x-card>
