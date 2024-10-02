d53df2cf852e1717eb95f25c908ac998e5d7322f
Tsaderu <106488407+Tsaderu@users.noreply.github.com>
2024-10-02 14:11:24 +0300

    0.283.3 Browser now don't autofill password and lastname in user crud page

--- resources/views/pages/cruds/users-crud.blade.php
+++ resources/views/pages/cruds/users-crud.blade.php
@@ -19,20 +19,19 @@
             <th><x-crud-components.activate-delete-btns-btn/></th>
         </tr>
         @foreach (array_merge($paginator->items(), $filler_rows) as $user)
-
             <tr data-row-id="{{ $user->id }}">
                 @php($is_exist_user = ("$user" !== ''))
                 <td><input type="text" value="{{ $user->name }}" @if($is_exist_user) disabled @endif onfocusout="update_cell_of(this)"></td>
                 <td><input type="text" value="{{ $user->surname }}" @if($is_exist_user) disabled @endif onfocusout="update_cell_of(this)"></td>
-                <td><input type="text" value="{{ $user->last_name }}" @if($is_exist_user) disabled @endif onfocusout="update_cell_of(this)"></td>
+                <td><input type="text" value="{{ $user->last_name }}" @if($is_exist_user) autocomplete="new-password" disabled @endif onfocusout="update_cell_of(this)"></td>
                 <td><x-crud-components.role-select :$user /></td>
 
-                <td><input type="password" value="{{ $is_exist_user ? '***' : '' }}" @if($is_exist_user) disabled @endif onfocusout="update_cell_of(this)"></td>
+                <td><input type="password" value="{{ $is_exist_user ? '***' : '' }}" @if($is_exist_user) autocomplete="new-password" disabled @endif onfocusout="update_cell_of(this)"></td>
                 <td><input type="email" value="{{ $user->email }}" @if($is_exist_user) disabled @endif onfocusout="update_cell_of(this)"></td>
                 <td><input type="text" value="{{ $user->phone_number }}" @if($is_exist_user) disabled @endif onfocusout="update_cell_of(this)"></td>
                 <td><input type="text" value="{{ $user->comment }}" onfocusout="update_cell_of(this)"></td>
                 <td>
-                    @can('delete ' . ("{$user->getRoleNames()->first()}" ?: 'user'))
+                    @can('delete ' . ("{$user->getRoleNames()->first()}" ?: 'user')) <!-- ?: 'user' guarantied that EmptyRow object users will be deletable -->
                         <x-crud-components.delete-btn/>
                     @endcan
                 </td>
